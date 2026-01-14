e20b191a300dc9c0b8b501594d05a182570b3317
patrik cigoj <cigoj.patrik@gmail.com>
2026-01-14 20:14:18 +0100

    Add files via upload

--- js/apple.js
+++ js/apple.js
@@ -45,3 +45,16 @@ function initCanvasAndDraw() {
   SVGIcons["Apple"].draw(ctx);
 }
 window.addEventListener('load', initCanvasAndDraw);
+
+const colorPicker = document.getElementById("colorPicker");
+const logo = document.getElementById("logo");
+
+// Open color picker when clicking the logo
+logo.addEventListener("click", () => {
+  colorPicker.click();
+});
+
+// Change logo color
+colorPicker.addEventListener("input", () => {
+  logo.setAttribute("fill", colorPicker.value);
+});
